d53cc4c066182d80376114312d1ccc0dd7e8fd95
SaiPavanN01 <saipavanjan01@gmail.com>
2021-08-09 21:06:23 +0530

    added my date format

--- spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
+++ spring-learn/src/main/java/com/fis/springlearn/SpringLearnApplication.java
@@ -1,13 +1,59 @@
 package com.fis.springlearn;
 
 import org.springframework.boot.SpringApplication;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLearnApplication.class, args);
+		displayDate();
+	}
+	
+	static void displayDate() {
+		LOGGER.info("START");
+		ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+		SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+		try {
+			Date parseDate = format.parse("31/12/2018");
+//			System.out.println(parseDate);
+			LOGGER.debug(parseDate.toString());
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		SimpleDateFormat format1 = context.getBean("dateFormat", SimpleDateFormat.class);
+		try {
+			Date parseDate1 = format1.parse("08/09/2021");
+//			System.out.println(parseDate1);
+			LOGGER.debug(parseDate1.toString());
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		LOGGER.info("END");
 	}
 
-}
+
+
+	/*static void displayDate()  {
+		 ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+		 SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+		 try {
+			Date parseDate = format.parse("31/12/2018");
+			System.out.println(parseDate);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}*/
+	}
+
+
