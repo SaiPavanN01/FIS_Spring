c72c0cea64bacc7447fa9cfcfe2e416d9401303a
SaiPavanN01 <saipavanjan01@gmail.com>
2021-08-11 19:39:30 +0530

    added employee jpa

---
+++ spring-jpa/java/com/fis/springjpa/employee/Employee.java
@@ -0,0 +1,34 @@
+package com.fis.springjpa.employee;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity // This tells Hibernate to make a table out of this class
+public class Employee {
+	
+	
+	  @Id
+	  @GeneratedValue(strategy=GenerationType.AUTO)
+	  int id;
+	  String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + "]";
+	}
+	  
+	  
+
+}
